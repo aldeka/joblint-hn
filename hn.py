545db58eac6578bb0f9ccbd0da86fc69ddea5af2
Karen Rustad <karen.rustad@gmail.com>
2013-10-01 18:10:43 -0700

    some code cleanup, added comments

--- hn.py
+++ hn.py
@@ -1,11 +1,13 @@
-from lxml import html
-from lxml.cssselect import CSSSelector
-from slugify import slugify
-import requests
 import os
 import shutil
 import subprocess
 from time import sleep
+
+import requests
+from lxml import html
+from lxml.cssselect import CSSSelector
+from slugify import slugify
+
 
 JOB_TABLE_INDEX = 2
 AD_DIR = 'job_ads'
@@ -13,6 +15,7 @@ URL = 'http://news.ycombinator.com/jobs'
 
 
 def lint_hn(download_afresh=False):
+    '''Runs joblint on a series of job ads acquired from Hacker News'''
     if download_afresh:
         parse_hn()
     filenames = os.listdir(AD_DIR)
@@ -23,18 +26,23 @@ def lint_hn(download_afresh=False):
 
 
 def parse_hn():
+    '''Looks for all the job links on the Hacker News jobs page, then downloads each of them and saves them to a directory'''
+    # Get the HN jobs page
     r = requests.get(URL)
 
     if r.status_code == 200:
         text = r.text
         jobs = get_job_links(text)
         try:
+            # directory where we're gonna store our ads
             os.mkdir(AD_DIR)
         except OSError:
             # Directory exists, let's clear it out
             shutil.rmtree(AD_DIR)
             os.mkdir(AD_DIR)
         for link in jobs:
+            # yay, synchronous downloading. 
+            # TODO: make this async somehow
             r = requests.get(link, timeout=30.0)
             if r.status_code != 200:
                 print "Warning, failed to get " + link
@@ -44,18 +52,26 @@ def parse_hn():
                 fout = open(AD_DIR + '/' + title + '.html', 'w')
                 fout.write(text.encode('utf-8'))
                 print "."
+                # let's not DDOS HN (or any other job site)
                 sleep(3)
         return
     else:
+        # oh noes
         print "Error: HTTP status code %s. Is Hacker News down?" % (r.status_code,)
 
 
 def get_job_links(text):
+    '''Finds HN job links and returns the URLs'''
     parsed = html.fromstring(text)
     sel = CSSSelector('table')
+    # Note: this is totally hacky, and could easily break if HN's layout changes
+    # TODO: figure out a better way to distinguish the job links-containing table (silly utterly unsemantic markup)
     job_table = sel(parsed)[JOB_TABLE_INDEX]
+
+    # we've got the table, now let's look for links in it
     sel = CSSSelector('a')
     links = []
+    
     for link in sel(job_table):
         url = link.get('href')
         if not url[:4] == 'http':
@@ -66,10 +82,12 @@ def get_job_links(text):
 
 
 def get_title(text):
+    '''Returns the title of an HTML page in slugify'd form'''
     parsed = html.fromstring(text)
     sel = CSSSelector('title')
     title = sel(parsed)[0].text_content()
     return slugify(title)
+
 
 if __name__=='__main__':
     lint_hn(download_afresh=True)
